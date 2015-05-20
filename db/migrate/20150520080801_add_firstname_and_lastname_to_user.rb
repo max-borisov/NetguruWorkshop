97fb6c3649188e5901066e9b610b894542ddbad9
max-borisov <max.borisov@yahoo.com>
2015-05-20 11:42:54 +0300

    Add firstname, lastname and admin columns to user. Fix user spec test.

---
+++ db/migrate/20150520080801_add_firstname_and_lastname_to_user.rb
@@ -0,0 +1,6 @@
+class AddFirstnameAndLastnameToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :firstname, :string, null: false, default: ""
+    add_column :users, :lastname, :string, null: false, default: ""
+  end
+end
