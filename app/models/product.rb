43779dd2aa2f33ac158724c935357f7beab77055
max-borisov <max.borisov@yahoo.com>
2015-05-28 15:48:01 +0300

    Update product model
    * fix product model tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,12 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates :description, :title, :price, presence: true
+  validates :price, numericality: true, format: { with: /\A\d{1}\.\d{1}\z/ }
+
+  def average_rating
+    all_ratings = self.reviews.map{ |e| e.rating }
+    all_ratings.sum.to_f / all_ratings.size
+  end
 end
