20080bb184cf9e38650e7670ce5523cfa13cafa0
max-borisov <max.borisov@yahoo.com>
2015-06-04 23:32:38 +0300

    Update product previews. Show username.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,12 @@ module ProductsHelper
   def review_belongs_to_current_user?(review)
     current_user.id == review.user_id
   end
+
+  def get_username_for_review(user)
+    if user.firstname.empty? || user.lastname.empty?
+      user.email.split('@').first
+    else
+      "#{user.firstname} #{user.lastname}"
+    end
+  end
 end
