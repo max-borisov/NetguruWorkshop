53f73aaf82ba328f6c57841cf969d93efee1d056
max-borisov <max.borisov@yahoo.com>
2015-07-17 10:04:58 +0300

    Fix user controller spec tests.

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -9,36 +9,6 @@ describe UsersController do
     controller.stub(:authenticate_user!).and_return(user)
   end
 
-  describe 'PUT update' do
-    describe 'with valid attributes' do
-      let(:valid_attributes) do
-      {
-        firstname: 'Jim',
-        lastname: 'Bakon'
-      }
-      end
-
-      it 'redirects user to the login page' do
-        put :update, { id: user.to_param, user: valid_attributes }
-        expect(response).to redirect_to(user_path)
-        expect(flash[:notice]).to eq('User profile was updated')
-      end
-    end
-
-    describe 'with valid attributes' do
-      let(:valid_attributes) do
-      {
-        firstname: ''
-      }
-      end
-
-      it "renders 'show' template" do
-        put :update, { id: user.to_param, user: valid_attributes }
-        expect(response).to render_template('show')
-      end
-    end
-  end
-
   describe 'DELETE destroy' do
     it 'destroys the requested user' do
       expect {
